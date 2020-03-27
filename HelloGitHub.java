4064e6662dadfe92017382791ed1a4028182e94a
victorromiux <42597105+victorromiux@users.noreply.github.com>
2020-03-26 23:44:26 -0600

    Greeting

---
+++ HelloGitHub.java
@@ -0,0 +1,6 @@
+class HelloGitHub{
+    public static void main(String args []){
+        System.out.println("Hello, GitHub!.\n"
+                + "My name is Victor Hugo, nice to meet you. Greetings from Mexico City.");
+    }
+}
